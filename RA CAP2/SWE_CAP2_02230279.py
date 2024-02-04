be46fac8434d7f3b4ee42e959c059304626e02f2
C-gyeltshen <02230279.cst@rub.edu.bt>
2024-02-04 23:38:07 +0600

    reading the file is done

---
+++ RA CAP2/SWE_CAP2_02230279.py
@@ -0,0 +1,45 @@
+################################
+# Your Name : Chimi Gyeltshen
+# Your Section : SWE
+# Your Student ID Number : 02230279
+################################
+# REFERENCES
+# Links that you referred while solving 
+# the problem
+# http://link.to.an.article/video.com 
+################################
+# SOLUTION
+# Your Solution Score:
+# Task 1: Put your solution here
+# Task 2: Put your solution here
+################################
+
+# # Read the input.txt file
+def read_input():
+    with open('RA CAP2/input_9_cap2.txt') as file:
+        print(file.read())
+
+        for line in file:
+            print (line.strip())
+
+        
+read_input()
+
+
+
+# solution
+# def task_1():
+    # implement your task 1 solution here.
+    # print your solution to output as: 
+    # "There were <number> people assigned and there are <number> of overlapping space         
+    # assignments"
+
+# def task_2():
+    # implement your task 2 solution here.
+    # print your solution to output as: 
+    # "There were <number> assignments that overlap completely"
+
+
+# Other parts of code here to run your functions and printing of the required solution.
+
+# Note: You may add parameters/arguments, return values to the functions above.
